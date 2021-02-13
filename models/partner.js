1f456dc304d3a67525f6bd67f5f9ff8c3d45262c
Your Name <epyon85@gmail.com>
2021-02-12 19:46:37 -0500

    Passport JWT

--- models/partner.js
+++ models/partner.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const partnerSchema = new Schem({
+const partnerSchema = new Schema({
     name: {
         type: String,
         required: true,
